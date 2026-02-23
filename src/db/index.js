7bc3a150bf54b04e5b79a3f53c365af6cc08f1f6
Umair Ali <umair.fullstackdev@gmail.com>
2026-02-24 02:54:47 +0500

    feat: add MongoDB connection and environment configuration
    - Updated package.json to include dotenv, express, and mongoose as dependencies.
    - Created src/constants.js to define the database name.
    - Implemented MongoDB connection logic in src/db/index.js.
    - Configured dotenv in src/index.js to load environment variables from .env file.

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"
+
+const connectDB = async () => {
+    try {
+        console.log("ENV:", process.env.MONGODB_URL)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MoongoDB connected !! DB HOST: ${connectionInstance.connection.host} `);
+        console.log(connectionInstance);
+
+    } catch (error) {
+        console.log("MoongoDB connection error ", error);
+        process.exit(1)
+    }
+}
+
+
+export default connectDB
