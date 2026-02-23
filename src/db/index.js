c24f1780d4600467d8d93240b0b265c4b59710b1
Umair Ali <umair.fullstackdev@gmail.com>
2026-02-24 03:50:44 +0500

    updated

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
         console.log("ENV:", process.env.MONGODB_URL)
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MoongoDB connected !! DB HOST: ${connectionInstance.connection.host} `);
-        console.log(connectionInstance);
 
     } catch (error) {
         console.log("MoongoDB connection error ", error);
