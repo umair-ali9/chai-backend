a2733af286d5aa0fedefa9f5b7a2366e88e90de1
Umair Ali <umair.fullstackdev@gmail.com>
2026-03-15 02:13:20 +0500

    feat: Add cookie-parser and cors middleware, implement ApiError and ApiResponse classes, and create asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -7,3 +7,11 @@ dotenv.config({
 import connectDB from "./db/index.js"
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at Port : ${process.env.PORT}`);
+    })
+  })
+  .catch((err) => {
+    console.log("Moongo DB connection failed!!!");
+  })
